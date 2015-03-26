dffd825c599ef21b7a74ca242302dbda03fddb4c
filipco56 <fico.hiki@gmail.com>
2015-03-26 18:53:40 +0100

    autocomplete

---
+++ search.js
@@ -0,0 +1,17 @@
+$(".searchbox").autocomplete({
+    source: function(request, response) {
+        console.log(request.term);
+        $.ajax({
+            url: "http://en.wikipedia.org/w/api.php",
+            dataType: "jsonp",
+            data: {
+                'action': "opensearch",
+                'format': "json",
+                'search': request.term
+            },
+            success: function(data) {
+                response(data[1]);
+            }
+        });
+    }
+});
